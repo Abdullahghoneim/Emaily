c3cb9a70e4c504a375fb05369b8641b8b92a7560
abdullahghoneim <abdullahghoneim890@gmail.com>
2019-08-02 23:21:20 +0200

    adding billong and client side app

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ app.use(passport.session());
 require("./routes/authRouter")(app);
 require("./routes/billingRouter")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true }).then(() => {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`app runing on port ${PORT}`));
